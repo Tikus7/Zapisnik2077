0c51a0a9c231c5725f42a5cafde178fc09edcf71
Tikus7 <tobikg@seznam.cz>
2020-10-28 18:04:52 +0100

    Initial commit

---
+++ Zapisnik2077/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Zapisnik2077
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public async void CreateFakeTable()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                var note = new Note();
+                note.Name = "Tobias";
+                note.Text = "Burger party";
+                note.CreationDate = new DateTime(1979, 07, 28);
+                note.LastChange = new DateTime(2020, 07, 28);
+                await App.Database.SaveNoteAsync(note);
+            }
+        }
+
+
+        async void CreateNewNote(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SingleNote());
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            //CreateFakeTable();
+            listView.ItemsSource = await App.Database.GetNotesAsync();
+            
+        }
+    }
+}
---
+++ Zapisnik2077/NoteDatabse.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using SQLite;
+
+namespace Zapisnik2077
+{
+    public class Note
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Text { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime LastChange { get; set; }
+    }
+
+    public class NoteDatabase
+    {
+        readonly SQLiteAsyncConnection _database;
+
+        public NoteDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Note>().Wait();
+        }
+
+        public Task<List<Note>> GetNotesAsync()
+        {
+            return _database.Table<Note>().ToListAsync();
+        }
+
+        public Task<Note> GetNoteAsync(int id)
+        {
+            return _database.Table<Note>()
+                            .Where(i => i.ID == id)
+                            .FirstOrDefaultAsync();
+        }
+
+        public Task<int> SaveNoteAsync(Note note)
+        {
+            if (note.ID != 0)
+            {
+                return _database.UpdateAsync(note);
+            }
+            else
+            {
+                return _database.InsertAsync(note);
+            }
+        }
+
+        public Task<int> DeleteNoteAsync(Note note)
+        {
+            return _database.DeleteAsync(note);
+        }
+    }
+}
